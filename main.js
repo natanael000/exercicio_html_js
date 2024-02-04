42ee08ed7539863bd45cc4ce01898985b9afa6b5
Natanael <Natanaelltda@gmail.com>
2024-02-04 19:48:32 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const form = document.getElementById('valid-form');
+
+let formvalid = false;
+
+isNaN(campoA, campoB); {
+    return campoB > campoA;
+}
+
+form.addEventListener('submit') , function (e) {
+    e.preventDefault();
+}
+let campoA = parseFloat(document.getElementById('n1').value);
+let campoB = parseFloat(document.getElementById('n2').value);
+
+if (Numbervalid(campoA, campoB)); {
+    document.querySelector('.erro-message').style.display = 'none';
+    document.querySelector('.success-message').style.display = 'block';
+    campoA.value = '';
+    campoB.value = '';
+} 
+else {
+    document.querySelector('.error-message').style.display = 'block';
+    document.querySelector('.success-message').style.display = 'none';
+    
+}
